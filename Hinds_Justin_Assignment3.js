906bd2861129b85010616ea4a2e20e0a538147b4
Justin-Hinds <justinhinds@gmail.com>
2015-01-21 21:25:33 -0800

    Working on nested loop and arrays

--- Hinds_Justin_Assignment3.js
+++ Hinds_Justin_Assignment3.js
@@ -4,10 +4,29 @@ var chef = "Chef Blackstock",
    sous= "Sous Chef Flynn",
    leadCook = "overton";
 var menu = ["Coq au Vin", "Beef Wellington", "Blackened Salmon"]
+var wellington = ["Duxelles", "Crepe", "Beef"]
+var coqAuVin = ["Wine", "Chicken", " Pearl Onions"]
+var salmon = ["Salmon", "Asparagus", "Garlic Mashed Potatoes"] 
 
 // global variables 
 
-var journey = function(){
+var journey = function(name){
+	console.log( name + " has left his house and went to the market to pick up the ingredients for tonights VIP table")
 
 }   
 
+var vipMenu = function(dish)
+{
+	for(var menuItems = 0; menuItems<menu.length; menuItems++)
+	{
+		var menuItem = menu[menuItems],
+			ingredients =  dish[menuItems]
+		console.log("start making " + menuItem+ "It Has" + ingredients)
+			for(entree=0; entree<dish.length; entree++)
+			{
+			console.log (ingredients)
+			}
+		}	
+	};
+	
+vipMenu(coqAuVin)
