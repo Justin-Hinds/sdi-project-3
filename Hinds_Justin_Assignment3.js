0b56fd0026a98d703e40ec71b52017c67a41e607
Justin-Hinds <justinhinds@gmail.com>
2015-01-21 18:13:41 -0800

    first set of variables

---
+++ Hinds_Justin_Assignment3.js
@@ -0,0 +1,13 @@
+/* */
+
+var chef = "Chef Blackstock",
+   sous= "Sous Chef Flynn",
+   leadCook = "overton";
+var menu = ["Coq au Vin", "Beef Wellington", "Blackened Salmon"]
+
+// global variables 
+
+var journey = function(){
+
+}   
+
