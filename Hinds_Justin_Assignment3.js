121ba671cc617757cff1e147c3581765233102fb
Justin-Hinds <justinhinds@gmail.com>
2015-01-21 22:49:10 -0800

    Save before array within array attempt

--- Hinds_Justin_Assignment3.js
+++ Hinds_Justin_Assignment3.js
@@ -2,21 +2,19 @@
 
 var chef = "Chef Blackstock",
    sous= "Sous Chef Flynn",
-   leadCook = "overton";
+   leadCook = "Overton";
 var menu = ["Coq au Vin", "Beef Wellington", "Blackened Salmon"]
 var wellington = ["Duxelles", "Crepe", "Beef"]
 var coqAuVin = ["Wine", "Chicken", " Pearl Onions"]
 var salmon = ["Salmon", "Garlic Mashed Potatoes","Asparagus"] 
-
+var served = 0
 // global variables 
 
 var journey = function(name){
 	console.log( name + " has left his house and went to the market to pick up the ingredients for tonights VIP table")
 
-}   
-
-var vipMenu = function(dish)
-{
+};   
+var vipMenu = function(dish){
 	for(var menuItems = 0; menuItems<menu.length; menuItems++)
 	{
 		var menuItem = menu[menuItems],
@@ -28,10 +26,18 @@ var vipMenu = function(dish)
 			}
 		}	
 	};
-
+var service = function(covers){
+	while(served < covers ){
+	
+	console.log( " We have " + ( covers - served) + "covers left!");
+	served += 10;
+	}
+		return served;
+};
 journey(chef)	
+service(80)
 vipMenu(coqAuVin)
 console.log( leadCook + '" I dropped the asparagus!!!"')
 var lostItem = salmon.pop()
 salmon.push("Veg Melody")
-console.log(salmon)
+console.log("Since " + leadCook + "decided to drop the asparagus the new build for the Blackened Salmon is " +salmon)
